0babf132cbcf7bbb1729d78deaffea793b2272d7
Julie <jmpexit@gmail.com>
2024-06-20 21:20:22 +0300

    2.3 login and logout are separated into different fixtures; session fixture validation is added

--- test/conftest.py
+++ test/conftest.py
@@ -2,10 +2,23 @@ import pytest
 from model.group import Group
 from fixture.application import Application
 
-@pytest.fixture (scope = "session") # т.о. обозначаем пайтесту, что эта функция - фикстура
+fixture = None
+@pytest.fixture # т.о. обозначаем пайтесту, что эта функция - фикстура
 def app(request):
-    fixture = Application() # функция создает (иниц-ет) фикстуру, объект Application, и возвращает ее
-    fixture.session.login(username="admin", passwd="secret")
+    global fixture
+    if fixture is None:
+        fixture = Application() # функция создает (иниц-ет) фикстуру, объект Application, и возвращает ее
+        fixture.session.login(username="admin", passwd="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", passwd="secret")
+    return fixture
+
+@pytest.fixture(scope="session", autouse=True)  #отдельная фикстура для финализации
+                                        # autouse=True означает, что фикстура сработает автоматически,
+                                        #несмотря на то, что не указана ни в одном из тестов
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
